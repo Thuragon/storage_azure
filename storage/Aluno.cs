7003dfe042fe928f860e9c8b6052b7e66acd611f
Logon Aluno <logonrmlocal@fiap.com.br>
2018-11-28 22:29:58 -0200

    Add project files.

---
+++ storage/Aluno.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace storage
+{
+    public class Aluno : TableEntity
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Aluno(string rm, string cidade) :base(cidade, rm)
+        {
+        }
+        public string PartitionKey { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string RowKey { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTimeOffset Timestamp { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ETag { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ storage/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace storage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+  
+        }
+
+        public void segundo()
+        {
+            CloudStorageAccount account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=storage13net;AccountKey=AhDlxuw3BZ3XhhFvgJUNmvVDBoiUZFXwrZoty/hLCiXdFzR2B/wXVnzlbS/dnRIt7oC/jn8mcvNYoaDluvercg==;EndpointSuffix=core.windows.net");
+            var client = account.CreateCloudTableClient();
+            var table = client.GetTableReference("rm331283");
+            table.CreateIfNotExistsAsync();
+
+            var entity = new Aluno("331283", "Sao Paulo");
+            entity.Nome = "Joao";
+            entity.Email = "nunsei@gmail.com";
+            table.ExecuteAsync(TableOperation.Insert(entity));
+
+            Console.Read();
+            Console.WriteLine("Hello World!");
+        }
+
+        public void primeiro()
+        {
+            CloudStorageAccount account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=storage13net;AccountKey=AhDlxuw3BZ3XhhFvgJUNmvVDBoiUZFXwrZoty/hLCiXdFzR2B/wXVnzlbS/dnRIt7oC/jn8mcvNYoaDluvercg==;EndpointSuffix=core.windows.net");
+            CloudBlobClient blobClient = account.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference("rm331283");
+            try
+            {
+                container.CreateIfNotExistsAsync();
+            }
+            catch { container.CreateIfNotExistsAsync().Wait(); }
+
+            var blob = container.GetBlockBlobReference("MyGod2.txt");
+            blob.UploadTextAsync("5+1= 10 acerto miseravi");
+
+            var sas = blob.GetSharedAccessSignature(new SharedAccessBlobPolicy()
+            {
+                Permissions = SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.Write,
+                SharedAccessExpiryTime = DateTime.Now.AddYears(5)
+            });
+
+            Console.WriteLine(blob.Uri + sas);
+            Console.Read();
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
